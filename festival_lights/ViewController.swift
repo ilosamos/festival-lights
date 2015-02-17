59da317db1c5e0b9aa48aedf7cff36e6534707b8
Daniel Berger <danielberger@Daniels-MacBook-Pro.local>
2015-02-17 19:43:23 +0100

    Initial Commit

---
+++ festival_lights/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  festival_lights
+//
+//  Created by Daniel Berger on 17/02/15.
+//  Copyright (c) 2015 Daniel Berger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
